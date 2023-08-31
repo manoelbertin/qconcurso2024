ffd33373d39ce2722ace722c2721fbbf8f96f3f3
manoel <manoeltirj@gmail.com>
2023-08-31 11:27:47 -0300

    Até aqui está funcionando, faltando implementar e testar o LOG_OFF.

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  namespace :site do
+    get 'welcome/index'
+  end
+  namespace :users_backoffice do
+    get 'welcome/index'
+  end
+  namespace :admins_backoffice do
+    get 'welcome/index'
+  end
+  devise_for :users
+  devise_for :admins
+  
+  get 'home', to: 'site/welcome#index'
+
+  root to: 'site/welcome#index'
+end
