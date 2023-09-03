aaee837f4699c495dd03805a1b00c5587dce2e7d
manoel <manoeltirj@gmail.com>
2023-09-03 19:13:15 -0300

    Na view onde listo os administradores terei um botão para editar, aqui fiz a action no admins_controller.

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -1,4 +1,9 @@
 class AdminsBackoffice::AdminsController < AdminsBackofficeController
+  # aqui está herdando tanto a autenticação quanto o layout específico.
   def index
+    @admins = Admin.all
+  end
+
+  def edit 
   end
 end
