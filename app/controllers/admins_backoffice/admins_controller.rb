a679891b9faf61da77bcccd1f0c11500e3c2f152
manoel <manoeltirj@gmail.com>
2023-09-04 20:47:09 -0300

    já tenho uma nova rota onde acrescentei em routes update, consequentemente renderizou a pág de edição de administradores, aproveitei e tbm criei uma action UPDATE.

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -1,9 +1,24 @@
 class AdminsBackoffice::AdminsController < AdminsBackofficeController
   # aqui está herdando tanto a autenticação quanto o layout específico.
+  before_action :set_admin, only: [:edit]
+
   def index
     @admins = Admin.all
   end
 
   def edit 
+  end
+
+  def update
+  end
+
+  private
+  
+  def params_admin
+    params.require(:admin).permit(:email, :password, :password_confirmation)
+  end
+
+  def set_admin
+    @admin = Admin.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   end
   namespace :admins_backoffice do
     get 'welcome/index'
-    resources :admins, only: [:index]
+    resources :admins, only: [:index, :edit, :update]
   end
   devise_for :admins
   devise_for :usuarios
