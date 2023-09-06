8d02696bf74c3b457c8fc7b8ce65e867898ae1bd
manoel <manoeltirj@gmail.com>
2023-09-06 07:16:08 -0300

    Copiei o mesmo formulário de edit para o new.html.erb alterando algum detalhe, a rota já apanhava o new e no controller criei a action create que foi parecida com a action    update.

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   end
   namespace :admins_backoffice do
     get 'welcome/index'
-    resources :admins, only: [:index, :edit, :update]
+    resources :admins, except: [:delete]
   end
   devise_for :admins
   devise_for :usuarios
