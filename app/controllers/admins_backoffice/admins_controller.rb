d238c02696f09b6a0b29afb9d0ed3b5107fbf552
manoel <manoeltirj@gmail.com>
2023-09-05 21:01:46 -0300

    Desfiz essa refatoração última porque o carregamento da função params_admin não precisa passar pelo before_action porque já vai carregar na função update.

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -2,7 +2,6 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
   # aqui está herdando tanto a autenticação quanto o layout específico.
   before_action :verify_password, only: [:update]
   before_action :set_admin, only: [:edit, :update]
-  before_action :params_admin, only: [:update]
 
   def index
     @admins = Admin.all.order(:id)
